cbcdd201a86d44519abad7b51c888fe5cf751c92
Peter Eisenlohr <peter@eisenlohr.org>
2025-06-22 21:02:39 +0000

    match slides

--- test/utils.cpp
+++ test/utils.cpp
@@ -10,18 +10,18 @@ size_t run(boost::asio::io_context& context)
    return context.run();
 #else
    size_t i = 0;
-   using namespace std::chrono;   
+   using namespace std::chrono;
    for (auto t0 = steady_clock::now(); context.run_one(); ++i)
    {
       auto t1 = steady_clock::now();
       auto dt = duration_cast<milliseconds>(t1 - t0);
-      t0 = t1;
       // clang-format off
       if (dt < 100ms)
          std::println("--- {} ------------------------------------------------------------------------", i);
       else
          std::println("\x1b[1;31m--- {} ({}) ----------------------------------------------------------------\x1b[0m", i, dt);
       // clang-format off
+      t0 = t1;
    }
    return i;
 #endif
