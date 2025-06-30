95fd6ee114ec9124e2580745d2840e059ba2e9f7
Peter Eisenlohr <peter@eisenlohr.org>
2025-06-30 20:09:32 +0000

    added testcase for boost::process SIGKILL issue

---
+++ src/boost_process_issue_503.cpp
@@ -0,0 +1,17 @@
+// https://github.com/boostorg/process/issues/503
+#include <boost/asio.hpp>
+#include <boost/process/v2/process.hpp>
+#include <iostream>
+
+int main()
+{
+   boost::asio::io_context context;
+   boost::process::v2::process child(context, "/usr/bin/sleep", {"10"});
+   child.async_wait([](boost::system::error_code ec, int status){
+      std::cout << status << std::endl;
+   });
+   child.terminate();  // prints 0 -- should be 9 (SIGKILL)
+   // child.interrupt();  // prints 2 (SIGINT)
+   // child.request_exit();  // prints 15 (SIGTERM)
+   context.run();
+}
