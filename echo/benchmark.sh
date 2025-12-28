222c2088a68487d92ad973ef5bbc309f2b45bbea
Peter Eisenlohr <peter@eisenlohr.org>
2025-12-28 13:54:40 +0000

    for completeness, added echo_async_threaded.cpp to benchmarks

--- echo/benchmark.sh
+++ echo/benchmark.sh
@@ -11,16 +11,15 @@ SERVERS=($(dirname "$0")/../build/echo/echo_*)
 SERVERS+=(echo/other/echo-go)
 SERVERS+=(echo/other/target/release/echo-rust)
 
-PATTERN='at ([0-9]+) MB'
+PATTERN='at ([0-9]+) MB/s'
 for ECHO in "${SERVERS[@]}"
 do
    "$ECHO" >/dev/null 2>&1 &
-   sleep 0.5
-   [[ $(build/bin/client -c 10 -d 10|grep Total) =~ $PATTERN ]]
+   sleep 0.5   
+   [[ $(build/bin/client -c 1 -d 1|grep Total) =~ $PATTERN ]]
    N=${BASH_REMATCH[1]}
    printf '%04d ' $N
-   N=$((N / 100))
-   for ((i = 0; i<N; i++)); do echo -n '='; done
+   for ((i = 0; i < N/100; i++)); do echo -n '='; done
    echo " ${ECHO##*/}"
    kill $!
    wait
---
+++ echo/echo_async_threaded.cpp
@@ -0,0 +1,70 @@
+#include <boost/asio.hpp>
+
+using boost::system::error_code;
+using namespace boost::asio;
+using ip::tcp;
+
+class session : public std::enable_shared_from_this<session>
+{
+public:
+   explicit session(tcp::socket socket) : socket_(std::move(socket)) {}
+   void start() { do_read(); }
+
+private:
+   void do_read()
+   {
+      auto self(shared_from_this());
+      socket_.async_read_some(buffer(data_), [this, self](error_code ec, std::size_t length)
+      {
+         if (!ec)
+            do_write(length);
+      });
+   }
+
+   void do_write(std::size_t length)
+   {
+      auto self(shared_from_this());
+      boost::asio::async_write(socket_, buffer(data_, length),
+                               [this, self](error_code ec, std::size_t /* length */)
+      {
+         if (!ec)
+            do_read();
+      });
+   }
+
+   tcp::socket socket_;
+   std::array<uint8_t, 64 * 1024> data_;
+};
+
+class server
+{
+public:
+   explicit server(tcp::acceptor acceptor) : acceptor_(std::move(acceptor)) { do_accept(); }
+
+private:
+   void do_accept()
+   {
+      acceptor_.async_accept([this](error_code ec, tcp::socket socket)
+      {
+         if (!ec)
+            std::make_shared<session>(std::move(socket))->start();
+
+         do_accept();
+      });
+   }
+
+   tcp::acceptor acceptor_;
+};
+
+int main()
+{
+   boost::asio::io_context context;
+   server server(tcp::acceptor{context, {tcp::v6(), 55555}});
+
+   std::vector<std::thread> threads(std::thread::hardware_concurrency());
+   for (auto& thread : threads)
+      thread = std::thread([&]() { context.run(); });
+   context.run();
+   for (auto& thread : threads)
+      thread.join();
+}
